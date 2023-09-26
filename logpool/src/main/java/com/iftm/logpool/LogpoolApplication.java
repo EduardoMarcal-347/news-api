e9307f770ba213f94c7d83c0ed637ce2a12bcc81
Eduardo Marçal <edumsfilho347@gmail.com>
2023-09-25 21:05:25 -0300

    adding logpool

---
+++ logpool/src/main/java/com/iftm/logpool/LogpoolApplication.java
@@ -0,0 +1,13 @@
+package com.iftm.logpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogpoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogpoolApplication.class, args);
+	}
+
+}
