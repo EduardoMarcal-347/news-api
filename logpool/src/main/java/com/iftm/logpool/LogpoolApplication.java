96b6ea3c600080f89ff9ea11875ab41fd2539580
Marçal <edumsfilho347@gmail.com>
2023-09-28 13:40:18 -0300

    updating logpool .yml

--- logpool/src/main/java/com/iftm/logpool/LogpoolApplication.java
+++ logpool/src/main/java/com/iftm/logpool/LogpoolApplication.java
@@ -1,9 +1,14 @@
 package com.iftm.logpool;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class LogpoolApplication {
 
 	public static void main(String[] args) {
