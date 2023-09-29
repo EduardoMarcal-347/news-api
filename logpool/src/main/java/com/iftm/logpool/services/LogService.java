53ab5319a15a41d2db5371400b7bb769cfc0338e
Eduardo Marçal <edumsfilho347@gmail.com>
2023-09-28 22:24:25 -0300

    adding last updates

--- logpool/src/main/java/com/iftm/logpool/services/LogService.java
+++ logpool/src/main/java/com/iftm/logpool/services/LogService.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
-import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
