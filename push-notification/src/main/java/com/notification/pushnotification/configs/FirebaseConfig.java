8c2f15bb5298d271f33004a42809e3caab8a06b0
Eduardo Marçal <edumsfilho347@gmail.com>
2023-10-23 20:51:02 -0300

    feature: creating FirebaseMessagingService

--- push-notification/src/main/java/com/notification/pushnotification/configs/FirebaseConfig.java
+++ push-notification/src/main/java/com/notification/pushnotification/configs/FirebaseConfig.java
@@ -1,4 +1,4 @@
-package com.pushnotification.configs;
+package com.notification.pushnotification.configs;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
