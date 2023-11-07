8d134a25e8e16607533e05f6a3fa028bc71f6f45
Eduardo Marçal <edumsfilho347@gmail.com>
2023-11-06 22:13:59 -0300

    chore: adding circuit fallback method

--- company-calculations/src/main/java/com/iftm/companycalculations/feign/FeignConsumer.java
+++ company-calculations/src/main/java/com/iftm/companycalculations/feign/FeignConsumer.java
@@ -1,6 +1,7 @@
 package com.iftm.companycalculations.feign;
 
 import com.iftm.companycalculations.entities.dto.EmployeeDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,5 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @FeignClient(value = "producer")
 public interface FeignConsumer {
     @GetMapping("api/v1/employee")
+    @CircuitBreaker(name = "consumer", fallbackMethod = "getResponse")
     ResponseEntity<EmployeeDto> getEmployee();
+
+    default ResponseEntity<String> getEmployee(Throwable t) {
+        var message = "Erro ao tentar acessar o microsserviço producer!";
+        return ResponseEntity.internalServerError().body("Fallback response!");
+    }
 }
